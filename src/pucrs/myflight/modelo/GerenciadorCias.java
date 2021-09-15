6ecfd824319fb29b951e0ed686a54e9037e366c0
aagustini <alexandre.agustini@pucrs.br>
2021-09-15 10:38:26 -0300

    Projeto demo do uso do git - versao inicial

---
+++ src/pucrs/myflight/modelo/GerenciadorCias.java
@@ -0,0 +1,14 @@
+package pucrs.myflight.modelo;
+
+import java.util.ArrayList;
+
+public class GerenciadorCias {
+	private ArrayList<CiaAerea> empresas;
+	
+	public GerenciadorCias() {
+		empresas = new ArrayList<>();
+	}
+
+	// primeiro gerenciador que vou fazer...
+	
+}
